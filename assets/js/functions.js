62f56013b69f9c6bd5029b70840271cee8274fa6
Bryan Summers <bsummersgb@gmail.com>
2017-01-08 17:07:28 +0800

    refactor js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,7 +3,7 @@ var temp;
 
 function getCoordinates() {
   $.ajax({
-    url: "http://ipinfo.io/json?",
+    url: "https://ipinfo.io/json?",
     dataType: "jsonp",
     data: "callback=JSON_CALLBACK",
     success: function( data ) {
@@ -11,9 +11,6 @@ function getCoordinates() {
       var lat = locationArray[0];
       var lon = locationArray[1];
       $('h2').html(data.city);
-
-
-      // $('.wrapper').html("Latitude: " + lat + " Longitude: " + lon);
 
         $.ajax({
           url: "http://api.openweathermap.org/data/2.5/weather?",
@@ -55,53 +52,13 @@ function getCoordinates() {
 
           }
         });
-
-
-
     }
   });
 }
 
-//print out city from ipInfo api - more accurate
-// print out temp
-//find out what values come under description
-
-
-
-
-// Celcius to farenheit conversion
-// Math.round( (celcius * 9)/5 + 32 )
-
-// My OpenWeather API Key
-// 9ec98b4ca89c6baa446ea30c23790416
-
-
-// Exact Url to make the API call (second one converts temp to celsius (default is kelvins) , 3rd to farenheit):
-// http://api.openweathermap.org/data/2.5/weather?lat=-31.9674&lon=115.8621&APPID=9ec98b4ca89c6baa446ea30c23790416
-// http://api.openweathermap.org/data/2.5/weather?lat=-31.9674&lon=115.8621&units=metric&APPID=9ec98b4ca89c6baa446ea30c23790416
-// http://api.openweathermap.org/data/2.5/weather?lat=-31.9674&lon=115.8621&units=imperial&APPID=9ec98b4ca89c6baa446ea30c23790416
-
-
-// Examples of API call(with coords, and by cityId):
-// http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=9ec98b4ca89c6baa446ea30c23790416
-// api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=9ec98b4ca89c6baa446ea30c23790416
-
-// http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=061f24cf3cde2f60644a8240302983f2
-
-
 $( document ).ready(function() {
 
   getCoordinates();
-
-  // $('.temperature').on('click', function(e) {
-  //   e.preventDefault();
-  //   var celcius =  parseInt( $('.temperature').text() );
-  //   console.log(celcius);
-  //   var farenheit = celcius * 9 / 5 + 32;
-  //
-  //   $('.temperature').text( farenheit );
-  //   console.log(celcius);
-  // })
 
   function setFarenheit() {
     return temp * 9 / 5 + 32;
@@ -119,10 +76,5 @@ $( document ).ready(function() {
     }
 
   })
-
-// c to f:   celcius×9/5+32
-// f to c:   (farenheit-32)×5/9
-
-
 
 });
